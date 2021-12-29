c0890d978bd98049e80b72cb03d54420e7888ea4
Rana Ziauddin <96728892+rzdin@users.noreply.github.com>
2021-12-29 19:30:27 +0500

    Update pipeline {.groovy

--- pipeline {.groovy
+++ pipeline {.groovy
@@ -17,7 +17,7 @@ pipeline {
         withAWS(credentials: 'AWSReservedSSO_AdministratorAccess_564bcbbbca5e5655/rzdin@enquizit.com', region: 'us-east-2') {
           unstash 'venv'
           unstash 'aws-sam'
-          sh 'venv/bin/sam package --stack-name $STACK_NAME -t template.yaml --s3-bucket $S3_BUCKET --capabilities CAPABILITY_IAM'
+          sh 'venv/bin/sam package --stack-name $STACK_NAME -t template.yaml --s3-bucket $S3_BUCKET --output-template-file gen/template-generated.yaml'
       }
     stage('beta') {
       environment {
