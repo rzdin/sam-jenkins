8d9b4121713ff35326b9997cb4bf3e5034cd092c
Rana Ziauddin <96728892+rzdin@users.noreply.github.com>
2021-12-29 19:29:03 +0500

    Update pipeline {.groovy

--- pipeline {.groovy
+++ pipeline {.groovy
@@ -8,6 +8,17 @@ pipeline {
         stash includes: '**/venv/**/*', name: 'venv'
       }
     }
+    stage('build') {
+      environment {
+        STACK_NAME = 'sam-app-beta-stage'
+        S3_BUCKET = 'sam-jenkins-demo-us-east-2-ranaziauddin'
+      }
+      steps {
+        withAWS(credentials: 'AWSReservedSSO_AdministratorAccess_564bcbbbca5e5655/rzdin@enquizit.com', region: 'us-east-2') {
+          unstash 'venv'
+          unstash 'aws-sam'
+          sh 'venv/bin/sam package --stack-name $STACK_NAME -t template.yaml --s3-bucket $S3_BUCKET --capabilities CAPABILITY_IAM'
+      }
     stage('beta') {
       environment {
         STACK_NAME = 'sam-app-beta-stage'
