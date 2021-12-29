55ec982485376a0a4bcb2ff871d822907da8f995
Rana Ziauddin <96728892+rzdin@users.noreply.github.com>
2021-12-29 18:52:10 +0500

    Update pipeline {.groovy

--- pipeline {.groovy
+++ pipeline {.groovy
@@ -6,7 +6,7 @@ pipeline {
       steps {
         sh 'python3 -m venv venv && venv/bin/pip install aws-sam-cli'
         stash includes: '**/venv/**/*', name: 'venv'
-    
+      }
     }
     stage('beta') {
       environment {
