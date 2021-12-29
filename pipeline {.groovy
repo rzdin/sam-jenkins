cc36b6a490b9a35ceed1bc92da3f26d80f03f401
Rana Ziauddin <rzdin@enquizit.com>
2021-12-29 15:35:55 +0500

    first commit

---
+++ pipeline {.groovy
@@ -0,0 +1,39 @@
+pipeline {
+  agent any
+ 
+  stages {
+    stage('Install sam-cli') {
+      steps {
+        sh 'python3 -m venv venv && venv/bin/pip install aws-sam-cli'
+        stash includes: '**/venv/**/*', name: 'venv'
+    
+    }
+    stage('beta') {
+      environment {
+        STACK_NAME = 'sam-app-beta-stage'
+        S3_BUCKET = 'sam-jenkins-demo-us-east-2-ranaziauddin'
+      }
+      steps {
+        withAWS(credentials: 'AWSReservedSSO_AdministratorAccess_564bcbbbca5e5655/rzdin@enquizit.com', region: 'us-easr-2') {
+          unstash 'venv'
+          unstash 'aws-sam'
+          sh 'venv/bin/sam deploy --stack-name $STACK_NAME -t template.yaml --s3-bucket $S3_BUCKET --capabilities CAPABILITY_IAM'
+          }
+        }
+      }
+    }
+    stage('prod') {
+      environment {
+        STACK_NAME = 'sam-app-prod-stage'
+        S3_BUCKET = 'sam-jenkins-demo-us-west-2-rana-ziauddin'
+      }
+      steps {
+        withAWS(credentials: 'AWSReservedSSO_AdministratorAccess_564bcbbbca5e5655/rzdin@enquizit.com', region: 'us-east-1') {
+          unstash 'venv'
+          unstash 'aws-sam'
+          sh 'venv/bin/sam deploy --stack-name $STACK_NAME -t template.yaml --s3-bucket $S3_BUCKET --capabilities CAPABILITY_IAM'
+        }
+      }
+    }
+  }
+}
